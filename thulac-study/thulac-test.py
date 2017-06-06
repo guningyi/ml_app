c2baa7da10fdccd351857f631f3e65fb36e2fb98
guningyi <ningyi.gu@ericsson.com>
2017-06-06 17:09:05 +0800

    test the thulac ,

---
+++ thulac-study/thulac-test.py
@@ -0,0 +1,5 @@
+import thulac
+
+if __name__ == '__main__':
+    thu1 = thulac.thulac();
+    thu1.cut_f("C:\\Users\\eniiguu\\Desktop\\thulac_test_input.txt", "C:\\Users\\eniiguu\\Desktop\\thulac_test_output.txt")
